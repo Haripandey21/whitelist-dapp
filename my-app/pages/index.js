52aa38274f024139c739156ab47977448c001b10
Hari Pandey <69706767+Haripandey21@users.noreply.github.com>
2022-06-29 22:58:57 -0700

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -150,22 +150,22 @@ export default function Home() {
     if (walletConnected) {
       if (joinedWhitelist) {
         return (
-          <div className={styles.description}>
+          <div class={styles.description}>
             Thanks for joining the Whitelist!
           </div>
         );
       } else if (loading) {
-        return <button className={styles.button}>Loading...</button>;
+        return <button classNameName={styles.button}>Loading...</button>;
       } else {
         return (
-          <button onClick={addAddressToWhitelist} className={styles.button}>
+          <button onClick={addAddressToWhitelist} classNameName={styles.button}>
             Join the Whitelist
           </button>
         );
       }
     } else {
       return (
-        <button onClick={connectWallet} className={styles.button}>
+        <button onClick={connectWallet} classNameName={styles.button}>
           Connect your wallet
         </button>
       );
@@ -178,7 +178,7 @@ export default function Home() {
   useEffect(() => {
     // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
     if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // Assign the Web3Modal className to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
         network: "rinkeby",
@@ -200,7 +200,7 @@ export default function Home() {
   <div className="max-w-7xl mx-auto">
     <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
       <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-        <polygon Namepoints="50,0 100,0 50,100 0,100" />
+        <polygon points="50,0 100,0 50,100 0,100" />
       </svg>
 
       <div>
@@ -223,7 +223,6 @@ export default function Home() {
       </div>
 
       <main className="mt-10 mx-auto max-w-7xl px-2 sm:mt-12 sm:px-6 md:mt-16 lg:mt-2 lg:px-8 xl:mt-3">
-      <main className="mt-10 mx-auto max-w-7xl px-2 sm:mt-12 sm:px-6 md:mt-16 lg:mt-2 lg:px-8 xl:mt-3">
         <div className="sm:text-center lg:text-left">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span className="block xl:inline"> Early Bird catches </span>
@@ -239,11 +238,11 @@ export default function Home() {
           </div>
           
           
-          <div className={styles.thanks}>
+          <div class={styles.thanks}>
                  {numberOfWhitelisted} have already joined the Whitelist
                  
           </div>
-          <div className={styles.thanks}>
+          <div class={styles.thanks}>
             {10-numberOfWhitelisted} Slots Remaining
             
           </div>
